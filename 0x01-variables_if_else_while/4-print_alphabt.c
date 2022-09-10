a898c5a26390f94da6770e8e2c470c09e77f7080
LolaIbikz <ibikunkle.oluwafunmilola@gmail.com>
2022-09-10 04:16:50 -0700

    print alphabet except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,11 @@ int main(void)
 
 	while (ch <= 'z')
 {
-	if (ch != 'e' && ch != 'q')
-	{
-		putchar(ch);
-	}
-	ch++
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
 	}
 	putchar('\n');
 	return (0);
