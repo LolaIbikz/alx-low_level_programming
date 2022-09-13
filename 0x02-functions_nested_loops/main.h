5db3023eb9dd33647f6409a3e415a56e66a1a85c
LolaIbikz <ibikunkle.oluwafunmilola@gmail.com>
2022-09-13 15:03:53 -0700

    lowercase alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 char _putchar(char c);
 void print_alphabet(void);
@@ -13,3 +13,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+#endif
