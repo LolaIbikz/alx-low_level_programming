df1c918b00deed1dc467046efa1b55c3e3923d18
LolaIbikz <ibikunkle.oluwafunmilola@gmail.com>
2022-09-13 12:24:33 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,11 @@
-#include "main.h"
-
+#include <unistd.h>
 /**
- *main - prints putchar
- *followed by a new line
- *Return: Always 0 (Success)
+ * main - prints putchar
+ *
+ * Return: Returns 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
+	write(1, "_putchar\n", 9);
 	return (0);
-}	
+}
