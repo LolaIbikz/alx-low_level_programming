cfaa2932da51ac5132cba28c98cc06bc6a74a328
LolaIbikz <ibikunkle.oluwafunmilola@gmail.com>
2022-10-06 11:59:02 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+void _puts(char*);
+void _putNum(char*, int);
+int _strlen(char*);
+void *malloc_checked(unsigned int);
+char *string_nconcat(char*, char*, unsigned int);
+void *_calloc(unsigned int, unsigned int);
+void setm_zero(char*, int);
+int *array_range(int, int);
+void *_realloc(void*, unsigned int, unsigned int);
+int multip(char*,char*, int, int, char*);
+
+#endif
