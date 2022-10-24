8e3e74385726226c98f02e6a59b06d69bdb7eebc
lolaibikz <ibikunle.oluwafunmilola@gmail.com>
2022-10-24 14:15:59 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+es (19 sloc)  323 Bytes
+/*
+ * File: 4-free_listint.c
+ * Auth: Funmi Ajayi
+ */
+
+#include "lists.h"
+
+/**
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list to be freed.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
